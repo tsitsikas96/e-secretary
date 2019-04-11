a4dbd8d7a9db7f2093e677b004cde46ff1e3df53
tsitsikas96 <tsitsikasilias@gmail.com>
2019-04-11 21:08:04 +0300

    Added path to diloseis

--- e_secretary/urls.py
+++ e_secretary/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
     path('change_avatar/', views.change_avatar, name='change-avatar'),
+    path('diloseis/', views.diloseis, name='diloseis'),
     path('my_courses/', views.my_courses, name='my_courses'),
     path('course/<int:didaskalia_id>/', views.course, name='course'),
     path('ergasies/<int:didaskalia_id>/', views.ergasies, name='ergasies'),
